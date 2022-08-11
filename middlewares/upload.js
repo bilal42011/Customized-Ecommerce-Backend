0b638caeb8e9e7270f6ac6d9bf6d51d4a887bb4a
Muhammad Safwan Karim <msafwankarim786@gmail.com>
2022-08-11 18:44:19 +0500

    seperate middlewares for images & other attachments

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,18 +1,43 @@
 const multer = require("multer");
 const path = require("path");
 
-const storage = multer.diskStorage({
+const imageStorage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads");
   },
   filename: (req, file, cb) => {
-    cb(
-      null,
-      `${Date.now()}-${file.fieldname}.${path.extname(file.originalname)}`
-    );
+    const supportedExtensions = [
+      ".jpg",
+      ".jpeg",
+      ".png",
+      ".gif",
+      ".webp",
+      ".bmp",
+    ];
+    const extension = path.extname(file.originalname);
+    if (supportedExtensions.includes(extension)) {
+      cb(null, `${Date.now()}-${file.fieldname}${extension}`);
+    } else {
+      cb(new Error("Unsupported file type"), null);
+    }
   },
 });
 
-const upload = multer({ storage: storage });
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    const extension = path.extname(file.originalname);
 
-module.exports = upload;
+    cb(null, `${Date.now()}-${file.fieldname}${extension}`);
+  },
+});
+
+const imageUpload = multer({ storage: imageStorage });
+const attachmentUpload = multer({ storage: fileStorage });
+
+module.exports = {
+  imageUpload,
+  attachmentUpload,
+};
