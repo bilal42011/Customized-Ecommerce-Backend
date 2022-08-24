0e6fc75ce2ef2fba83f54dbb014004c706f420ba
Muhammad Safwan Karim <msafwankarim786@gmail.com>
2022-08-25 04:00:31 +0500

    enabled time stamps for buyer requests

--- models/BuyerRequest.js
+++ models/BuyerRequest.js
@@ -1,54 +1,59 @@
 const mongoose = require("mongoose");
 
-const buyerRequestSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  attachments: [
-    {
-      filename: {
-        type: String,
-        required: true,
-      },
-      path: {
-        type: String,
-        required: true,
-      },
+const buyerRequestSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-  ],
-  status: {
-    type: String,
-    enum: ["OPEN", "ACTIVE_ORDER", "CLOSED"],
-    default: "OPEN",
-  },
-  budget: {
-    type: Number,
-    min: [1, "Price must be greater than 0"],
-  },
-  deliveryTime: {
-    type: Number,
-    min: [1, "Invalid timeline"],
-  },
-  buyerId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-  proposals: [
-    {
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    attachments: [
+      {
+        filename: {
+          type: String,
+          required: true,
+        },
+        path: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    status: {
+      type: String,
+      enum: ["OPEN", "ACTIVE_ORDER", "CLOSED"],
+      default: "OPEN",
+    },
+    budget: {
+      type: Number,
+      min: [1, "Price must be greater than 0"],
+    },
+    deliveryTime: {
+      type: Number,
+      min: [1, "Invalid timeline"],
+    },
+    buyerId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Proposal",
+      ref: "User",
     },
-  ],
-});
+    proposals: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Proposal",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const BuyerRequest = mongoose.model("BuyerRequest", buyerRequestSchema);
 module.exports = BuyerRequest;
